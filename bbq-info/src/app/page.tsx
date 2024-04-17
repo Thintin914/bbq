3deb57f1f158474ddef575bb0cf6f403e8c21f4a
Thintin <ttca914@gmail.com>
2024-04-17 22:12:47 +0800

    t

--- bbq-info/src/app/page.tsx
+++ bbq-info/src/app/page.tsx
@@ -17,11 +17,11 @@ export default function page() {
         <p>and then walk to my bbq place within 10 minutes!</p>
       </div>
       <p className=" mt-5 font-semibold">Walk from bus station:</p>
-      <p>Get out of bus station, walk straight til the end. You can see a roundabout there.</p>
-      <p>Go downhill at Po Kak Tsai Road, and then walk at the left hand side.</p>
+      <p>Get out of bus station, walk straight til the end of Lung Ma Road (龍馬路). You can see a roundabout there.</p>
+      <p>Go downhill at Po Kak Tsai Road (布格仔路), and then walk at the left hand side.</p>
       <p>Walk straight without going to intersect roads.</p>
       <p>Be careful of vehicles because there&#39;s no pedestrian road.</p>
-      <p>At Lau Shui Heung Road, go right hand side.</p>
+      <p>At Lau Shui Heung Road (流水嚮道), go right hand side.</p>
       <p>Walk straight and there you go!</p>
       <iframe src="https://www.google.com/maps/d/embed?mid=1ExqA4WYqcw9tVI7jkNnxIcyicNhaP6I&ehbc=2E312F" width="640" height="480"></iframe>
     </div>
