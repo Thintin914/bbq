3dd9b284d7d2e4112811762cb02c6f2976f87667
Thintin <ttca914@gmail.com>
2024-04-17 21:04:08 +0800

    t

--- bbq-info/src/app/page.tsx
+++ bbq-info/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <p className=" font-semibold mb-5 text-lg">Yeaaa BBQ!</p>
       <p>Location:</p>
       <iframe src="https://www.google.com/maps/embed?pb=!4v1713358438439!6m8!1m7!1shREAn5wpGTq1DEXrKIf8eg!2m2!1d22.50099069488498!2d114.1671546107569!3f189.9881867028298!4f-8.677111205758791!5f0.7820865974627469" width="600" height="450" loading="lazy"></iframe>
-      <Link className=" underline cursor-pointer" href={'https://www.google.com/maps/@22.5009907,114.1671546,3a,75y,189.99h,81.32t/data=!3m9!1e1!3m7!1shREAn5wpGTq1DEXrKIf8eg!2e0!7i16384!8i8192!9m2!1b1!2i46?entry=ttu'}>Link Here!</Link>
+      <Link className=" underline cursor-pointer" href={'https://maps.app.goo.gl/mMLWgSNHLUG5xRFP8'}>Link Here!</Link>
       <p className=" mt-5">Traffic:</p>
       <div className=" inline-flex justify-center items-center gap-2">
         <p>You can take</p>
