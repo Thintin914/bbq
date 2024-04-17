79e210cd53fe4ca6b9a9f9a4aacd5b6e74f8bae1
Thintin <ttca914@gmail.com>
2024-04-17 21:08:37 +0800

    t

--- bbq-info/src/app/page.tsx
+++ bbq-info/src/app/page.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function page() {
   return (
     <div className=" w-full min-h-screen h-fit bg-[#9DCDF1] text-white p-2 flex flex-col justify-center items-center">
       <p className=" font-semibold mb-5 text-lg">Yeaaa BBQ!</p>
       <p>Location:</p>
-      <Link className=" underline cursor-pointer" href={'https://maps.app.goo.gl/mMLWgSNHLUG5xRFP8'}>Link Here!</Link>
+      <Link className=" underline cursor-pointer" href={"https://maps.app.goo.gl/mMLWgSNHLUG5xRFP8"}>Link Here!</Link>
       <p className=" mt-5">Traffic:</p>
       <div className=" inline-flex justify-center items-center gap-2">
         <p>You can take</p>
-        <Link href={'https://hkbus.fandom.com/wiki/%E5%9F%8E%E5%B7%B479X%E7%B7%9A'} className=" underline cursor-pointer">Bus 79X</Link>
+        <Link href={"https://hkbus.fandom.com/wiki/%E5%9F%8E%E5%B7%B479X%E7%B7%9A"} className=" underline cursor-pointer">Bus 79X</Link>
         <p>to go to the last station - 皇后山邨總站 Queen's Hill Estate B/T,</p>
         <p>and then walk to my bbq place within 10 minutes!</p>
       </div>
